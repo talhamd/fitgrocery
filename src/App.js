7174d029fc8938d11f255522297620a215b05c19
talhamd <talhauddin78@gamil.com>
2023-12-14 11:45:39 +0530

    adding src files

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Route, Routes, BrowserRouter} from 'react-router-dom';
+import Home from './PAGES/HOME/Home';
+import Contact from './PAGES/HOME/Contact';
+
+
+const App = () => {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element = {<Home/>}/>
+      <Route path='/Home' element = {<Home/>} />
+      <Route path='/Contact' element = {<Contact/>} />
+      <Route path='*' element = {<div>
+        <h1>404 NOT FOUND</h1>
+      </div>} />
+    </Routes>
+    </BrowserRouter>
+ 
+  )
+}
+export default App;
---
+++ src/PAGES/HOME/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Navbar from '../../COMPONENTS/Navbar.js'
+
+const Home = () => {
+  return (
+    <Navbar/>
+  )
+}
+export default Home;
